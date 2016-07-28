b0884c4cd1f407eb0b9403906f0e47757994a306
ravkovskiy <kos1992@mail.ru>
2016-07-28 20:42:43 +0300

    first using $scope, create simple store's list

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,22 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+  .controller('View1Ctrl', ['$scope', function ($scope) {
+    $scope.stores = [
+      {
+        name: '35Element',
+        adress: 'Olshevskogo, 1'
+      },
+      {
+        name: 'New Time',
+        adress: 'Prityckogo, 2'
+      }
+    ];
+  }]);
