a7aa76aa87ba16c6f6d79eec25b3919d0ba4fa13
ravkovskiy <kos1992@mail.ru>
2016-07-30 11:49:47 +0300

    delete comments

--- app/app.js
+++ app/app.js
@@ -21,9 +21,3 @@ angular.module('app', ['ngComponentRouter', 'stores'])
   ]
 });
 })(window.angular);
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
