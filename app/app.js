d367b1030797ee22ad79d46c01e977d3eeb52ea1
ravkovskiy <kos1992@mail.ru>
2016-07-31 16:27:56 +0300

    delete unless links

--- app/app.js
+++ app/app.js
@@ -3,17 +3,18 @@
 angular.module('app', ['ngComponentRouter', 'stores'])
 
 .config(function($locationProvider) {
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(false);
 })
 
 .value('$routerRootComponent', 'app')
 
 .component('app', {
   template:
-    '<ng-outlet></ng-outlet>',
+    '<hr><ng-outlet></ng-outlet>',
   $routeConfig: [
     {path: '/...', name: 'Stores', component: 'stores', useAsDefault: true},
     
   ]
 });
 })(window.angular);
+
